83713617846af63ba6c92cd664cdeec61d8c6106
Vamsijani <117339668+Vamsijani@users.noreply.github.com>
2022-11-06 19:58:21 +0530

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Springboot..!!!";
+        return "Hi Janakiraman, welcome to banglore..!!!";
     }
     
 }
